ccee1ef579789144df25429c17a5800a55d02f9c
jose <josenochebuena052@gmail.com>
2022-08-25 22:38:42 -0600

    correccion de librerias

--- src/clases/Sonido.java
+++ src/clases/Sonido.java
@@ -1,13 +1,9 @@
 package clases;
 
 import java.io.File;
-import java.io.IOException;
+
+
 import javafx.embed.swing.JFXPanel;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import sonidos.Ruta;
